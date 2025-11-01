172b4352ab0a2bffd0a2306cc0a8e06b1a71bc21
Lakindu Ravishka <237532771+Lakindu-R@users.noreply.github.com>
2025-11-01 23:03:03 +0530

    partners page mobile responsive complete

--- src/pages/Partners/sections/partners.tsx
+++ src/pages/Partners/sections/partners.tsx
@@ -1,11 +1,12 @@
-import spookies3 from '../../../../public/spookies.png';
+import spookies from '../../../../public/spookies.png';
+import spookies2 from '../../../../public/spookies2.png';
+import Mountains from '../../../../public/mountains.png';
 import Bg from '../../../../public/PathtoBuy/bg.png';
 
 function partners() {
   return (
     <section className="relative mx-auto h-[95vh] w-full overflow-hidden bg-linear-to-b from-[#0a1628] via-[#1a3a5c] to-[#2a4a6c]">
       <div className="mx-auto max-w-7xl">
-        
         <div className="absolute right-0 bottom-0 left-0 z-0">
           <img
             src={Bg}
@@ -13,26 +14,34 @@ function partners() {
             className="h-auto w-full object-cover lg:h-[80vh]"
           />
         </div>
-
-        
-        <div className="absolute top-1/2 left-1/2 z-10 -translate-x-1/2 -translate-y-1/2">
-          <div className="relative flex items-center justify-center">
-            
-            <img
-              src={spookies3}
-              alt="Spooky Character"
-              className="lg:h-screen brightness-[0.5]"
-            />
-          </div>
+        <div className="absolute right-0 bottom-[-20%] z-20">
+          <img
+            src={Mountains}
+            alt="Background"
+            className="md:w-[70vw] lg:w-[60vw]"
+          />
+        </div>
+        {/* Spookies only show large screen */}
+        <div className="absolute right-[14%] -bottom-[7%] hidden items-center justify-center lg:block">
+          <img
+            src={spookies}
+            alt="Spooky Character"
+            className="w-[900px] lg:h-screen brightness-75"
+          />
+        </div>
+        {/* Spookies  only show small screen */}
+        <div className="absolute bottom-0 left-1/2 flex h-[60vh] w-[200vw] -translate-x-1/2 lg:hidden">
+          <img
+            src={spookies}
+            alt="Spooky Character"
+            className="h-full w-full object-contain"
+          />
         </div>
 
-     
-        <div className="relative z-20 mx-auto px-4 py-12 sm:px-6 sm:py-16 lg:px-0 ">
-          <div className="flex items-center">
-            
-
-            <div className="absolute top-0 h-[90vh] w-[40vw] content-center text-center -left-[22vh]">
-              <h1 className="Mansalva text-2xl leading-tight font-normal text-white sm:text-3xl md:text-4xl lg:text-5xl xl:text-[7vh]">
+        <div className="relative z-20 mx-auto px-4 pt-[23%] md:pt-[15%] sm:px-6 sm:py-16 lg:px-0 ">
+          <div className="flex items-center justify-center lg:justify-start lg:items-start lg:w-[584px]">
+            <div className=" text-center   ">
+              <h1 className="Mansalva text-2xl leading-tight font-normal text-white sm:text-3xl md:text-2xl lg:text-[5vh] ">
                 We are always open for partnerships. If you are interested,
               </h1>
               <p className="Mansalva text-xl font-normal text-white sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl">
